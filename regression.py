235c371d93983de3600c7f0af9890ebcf83b5b09
DeleteMemory <xtliudexin@gmail.com>
2017-12-04 16:34:59 +0800

    stacking finish

--- regression.py
+++ regression.py
@@ -32,7 +32,7 @@ lsr_alpha = 0.0005547
 enr_alpha = 0.0009649
 enr_l1r = 0.5
 
-rand_seed = 33
+rand_seed = 2017
 fill_in_gpa = 2.35815726
 
 ori_one_hot_columns = ['province', 'gender', 'test_year', 'nation', 'politics', 'color_blind',
@@ -173,14 +173,12 @@ proc_data = proc_data.drop(drop_columns,axis=1)
 
 # standardization
 ss_x = prep.StandardScaler()
-ss_y = prep.StandardScaler()
 proc_data[numerical_columns] = ss_x.fit_transform(proc_data[numerical_columns].values)
 
 # spilt training data
 x_all_train = proc_data[proc_data['test_tag']!='test'].drop(other_columns,axis=1)
 x_test = proc_data[proc_data['test_tag'] == 'test'].drop(other_columns, axis=1)
 result_data = proc_data['GPA'][proc_data['test_tag']!='test']
-y_all_train = ss_y.fit_transform(result_data.values.reshape(-1,1)).ravel()
 y_all_train = result_data.values
 x_train,x_valid,y_train,y_valid = train_test_split(x_all_train.values,y_all_train,random_state=rand_seed)
 
@@ -195,19 +193,15 @@ class Stacking(object):
         self.stacker = stacker
         self.base_models = base_models
 
-    def fit_predict(self, X, y, T):
+    def fit(self, X, y):
         X = np.array(X)
         y = np.array(y)
-        T = np.array(T)
 
         kf = KFold(n_splits=self.n_folds, shuffle=True, random_state=rand_seed)
 
         s_train = np.zeros((X.shape[0], len(self.base_models)))
-        s_test = np.zeros((T.shape[0], len(self.base_models)))
 
         for i, mod in enumerate(self.base_models):
-            s_test_i = np.zeros((s_test.shape[0], kf.get_n_splits()))
-
             j = 0
             for idx_train, idx_valid in kf.split(range(len(y))):
                 x_train_j = X[idx_train]
@@ -218,33 +212,36 @@ class Stacking(object):
 
                 y_valid_j = mod.predict(x_valid_j)[:]
                 s_train[idx_valid, i] = y_valid_j
-                s_test_i[:, j] = mod.predict(T)[:]
 
                 j += 1
 
-            s_test[:, i] = s_test_i.mean(1)
-
         self.stacker.fit(s_train, y)
+
+    def predict(self,T):
+        T = np.array(T)
+        s_test = np.zeros((T.shape[0], len(self.base_models)))
+
+        for i, mod in enumerate(self.base_models):
+            s_test[:, i] = mod.predict(T)[:]
+
         y_predict = self.stacker.predict(s_test)[:]
-        print(s_train[0])
-        print(y[0])
-        print(s_test[0])
-        print(y_predict[0])
 
         return y_predict
+
+
 #%% SVR
 # svr_score = -cross_val_score(svr,x_all_train,y_all_train,cv=5,scoring='neg_mean_squared_error')
 # svr.fit(x_all_train, y_all_train)
-# svr_y_all_predict = ss_y.inverse_transform(svr.predict(x_all_train))
-# svr_y_test_predict = ss_y.inverse_transform(svr.predict(x_test))
+# svr_y_all_predict = svr.predict(x_all_train)
+# svr_y_test_predict = svr.predict(x_test)
 # print("svr_valid_mse: {}".format(svr_score.mean()))
 # print("svr_all_mse: {}".format(metrics.mean_squared_error(result_data,svr_y_all_predict)))
 
 #%% Ridge regression
 # regr_score = -cross_val_score(regr,x_all_train,y_all_train,cv=5,scoring='neg_mean_squared_error')
 # regr.fit(x_all_train, y_all_train)
-# regr_y_all_predict = ss_y.inverse_transform(regr.predict(x_all_train))
-# regr_y_test_predict = ss_y.inverse_transform(regr.predict(x_test))
+# regr_y_all_predict = regr.predict(x_all_train)
+# regr_y_test_predict = regr.predict(x_test)
 # print("regr_valid_mse: {}".format(regr_score.mean()))
 # print("regr_all_mse: {}".format(
 #     metrics.mean_squared_error(result_data, regr_y_all_predict)))
@@ -252,8 +249,8 @@ class Stacking(object):
 #%% Lasso regression
 # lsr_score = -cross_val_score(lsr,x_all_train,y_all_train,cv=5,scoring='neg_mean_squared_error')
 # lsr.fit(x_all_train, y_all_train)
-# lsr_y_all_predict = ss_y.inverse_transform(lsr.predict(x_all_train))
-# lsr_y_test_predict = ss_y.inverse_transform(lsr.predict(x_test))
+# lsr_y_all_predict = lsr.predict(x_all_train)
+# lsr_y_test_predict = lsr.predict(x_test)
 # print("lsr_valid_mse: {}".format(lsr_score.mean()))
 # print("lsr_all_mse: {}".format(
 #     metrics.mean_squared_error(result_data, lsr_y_all_predict)))
@@ -261,25 +258,22 @@ class Stacking(object):
 #%% Elastic Net regression
 # enr_score = -cross_val_score(enr,x_all_train,y_all_train,cv=5,scoring='neg_mean_squared_error')
 # enr.fit(x_all_train, y_all_train)
-# enr_y_all_predict = ss_y.inverse_transform(enr.predict(x_all_train))
-# enr_y_test_predict = ss_y.inverse_transform(enr.predict(x_test))
+# enr_y_all_predict = enr.predict(x_all_train)
+# enr_y_test_predict = enr.predict(x_test)
 # print("enr_valid_mse: {}".format(enr_score.mean()))
 # print("enr_all_mse: {}".format(
 #     metrics.mean_squared_error(result_data, enr_y_all_predict)))
 
 #%% 5-ford stacking
-stacking = Stacking(n_folds=5,stacker=lm.Ridge(alpha=0.1),base_models=[enr,svr,regr,lsr])
-# stacking_y_valid_predict = ss_y.inverse_transform(stacking.fit_predict(
-#     X=x_train, y=y_train, T=x_valid))
-stacking_y_valid_predict = (stacking.fit_predict(
-    X=x_train, y=y_train, T=x_valid))
-stacking_y_all_predict = (stacking.fit_predict(
-    X=x_all_train, y=y_all_train, T=x_all_train))
-print("stacking_valid_mse: {}".format(metrics.mean_squared_error(y_valid,stacking_y_valid_predict)))
+stacking = Stacking(n_folds=5,stacker=lm.Ridge(alpha=11.0),base_models=[enr,svr,regr,lsr])
+stacking.fit(x_train,y_train)
+stacking_y_valid_predict = stacking.predict(x_valid)
+print("stacking_valid_mse: {}".format(metrics.mean_squared_error(y_valid, stacking_y_valid_predict)))
+stacking.fit(x_all_train, y_all_train)
+stacking_y_all_predict = stacking.predict(x_all_train)
 print("stacking_all_mse: {}".format(
     metrics.mean_squared_error(y_all_train, stacking_y_all_predict)))
-# stacking_y_test_predict = ss_y.inverse_transform(stacking.fit_predict(
-#     X=x_all_train, y=y_all_train, T=x_test))
+stacking_y_test_predict = stacking.predict(x_test)
 
 #%% save result
 # result = proc_data[['student_ID','GPA']][proc_data['test_tag']=='test']
@@ -322,12 +316,12 @@ print("stacking_all_mse: {}".format(
 # result.to_csv('result/result_{}_enr.csv'.format(time.strftime("%b_%d_%H-%M-%S",time.localtime())),
 #             header=True,index=False,encoding='utf-8')
 
-# result = proc_data[['student_ID','GPA']][proc_data['test_tag']=='test']
-# result['GPA'] = stacking_y_test_predict
-# result.columns=['学生ID','综合GPA']
-# insert_line = pd.DataFrame([['40dc29f67d3a0ea205e4',fill_in_gpa]],columns=['学生ID','综合GPA'])
-# above_result = result[:58]
-# below_result = result[58:]
-# result = pd.concat([above_result,insert_line,below_result],ignore_index=True)
-# result.to_csv('result/result_{}_stacking.csv'.format(time.strftime("%b_%d_%H-%M-%S",time.localtime())),
-#             header=True,index=False,encoding='utf-8')
+result = proc_data[['student_ID','GPA']][proc_data['test_tag']=='test']
+result['GPA'] = stacking_y_test_predict
+result.columns=['学生ID','综合GPA']
+insert_line = pd.DataFrame([['40dc29f67d3a0ea205e4',fill_in_gpa]],columns=['学生ID','综合GPA'])
+above_result = result[:58]
+below_result = result[58:]
+result = pd.concat([above_result,insert_line,below_result],ignore_index=True)
+result.to_csv('result/result_{}_stacking.csv'.format(time.strftime("%b_%d_%H-%M-%S",time.localtime())),
+            header=True,index=False,encoding='utf-8')
